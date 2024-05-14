6ae042724510c2f07bf06ae9729aef928f8e5127
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-14 21:52:40 +0500

    add dotenv

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
-
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import MoviesCard from './MoviesCard';
-import searchIcon from './search.svg'
+import searchIcon from './search.svg';
+import dotenv from 'dotenv';
 
-
-const API_URL = "http://www.omdbapi.com?apikey=dfb2c155";
+// Load environment variables from .env file
+dotenv.config();
 
 function App() {
-  const [movies, setMovies]=useState([])
-  const[searchTerm, setSearchTerm]=useState()
-  const searchMovies=async(title)=>{
-    const response=await fetch (`${API_URL}&s=${title}`)
-    const data=await response.json();
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    setMovies(data.Search)
-   
-  }
+  const searchMovies = async (title) => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
 
-  useEffect(()=>{
-    searchMovies('Spiderman')
-  },[])
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
   return (
-  
     <div className="App">
       <h1>Movies Chart</h1>
       <div className="search">
-        <input type="text" placeholder='Search for movies' 
-        value={searchTerm}
-        onChange={(e)=>setSearchTerm(e.target.value)}/>
-        <img src={searchIcon} alt="Search" 
-        onClick={()=>searchMovies(searchTerm)}/>
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={searchIcon} alt="Search" onClick={() => searchMovies(searchTerm)} />
       </div>
-      {
-        movies?.length>0
-        ?(
-          <div className="container">
-       { movies.map((movie)=>(
-          <MoviesCard movie={movie}/>
-        ))}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MoviesCard key={movie.imdbID} movie={movie} />
+          ))}
         </div>
-
-        ):
-        (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )
-      }
-     
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
